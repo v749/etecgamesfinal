7c26499fc992b1ff1b078aa01e87dacb7d83aa9a
v479 <victoroliveira74927@gmail.com>
2021-10-25 16:45:29 -0300

    Comentario na Controller Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
+	public function index() //Função no qual vai mostrar as nossas pgs principais por default
 	{   		
 		echo view('header');		
 		echo view('footer');		 
